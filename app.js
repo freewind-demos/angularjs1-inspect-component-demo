e028b9a1d07926002b5951a07c7479eb9a980ab0
freewind <nowindlee@gmail.com>
2018-09-02 19:12:44 +0800

    demo

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
 const app = angular.module('app', [])
 app.controller('MyController', function ($scope) {
-    $scope.yourName = 'AngularJS'
+    $scope.count = 5
+})
+
+app.component('componentCounter', {
+    template: `
+    <div>
+        <span>Number: {{ $ctrl.count }}</span>
+        <button ng-click="$ctrl.increase()">+</button>
+        <button ng-click="$ctrl.decrease()">-</button>
+    </div>
+    `,
+    bindings: {
+        count: '='
+    },
+    controller: function () {
+        this.increase = function () {
+            this.count += 1
+        }
+        this.decrease = function () {
+            this.count -= 1
+        }
+    }
+
+})
+
+app.directive('directiveCounter', function () {
+    return {
+        restrict: 'E',
+        template: `
+         <div>
+             <span>Number: {{ $ctrl.count }}</span>
+             <button ng-click="$ctrl.increase()">+</button>
+             <button ng-click="$ctrl.decrease()">-</button>
+         </div>
+         `,
+        scope: {},
+        bindToController: {
+            count: '='
+        },
+        controller: function () {
+            this.increase = function () {
+                this.count += 1
+            }
+            this.decrease = function () {
+                this.count -= 1
+            }
+        },
+        controllerAs: '$ctrl'
+    }
 })
